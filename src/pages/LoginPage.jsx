cddd475798a84b1805ac91989334dd6761b95d80
muxammadazizjon <mazizmaxamatov@gmail.com>
2025-03-18 15:39:52 +0500

    "zzzz"

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -23,7 +23,7 @@ const LoginPage = () => {
     };
 
     return (
-        <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <div className=" w-[800px] mx-auto flex min-h-screen items-center justify-center bg-gray-100">
             <div className="bg-white p-8 rounded-2xl shadow-lg w-96">
                 <h2 className="text-2xl font-bold text-center mb-6">Tizimga kirish</h2>
                 {error && <p className="text-red-500 text-center">{error}</p>}
